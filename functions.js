952f554c022abc9c42098e73e8a466a9ef1b8263
priyanka9634 <102957031+priyanka9634@users.noreply.github.com>
2023-03-24 12:46:24 +0530

    minor fixes

--- functions.js
+++ functions.js
@@ -3,9 +3,7 @@ function loadWindow(){
 
 
 window.onload = (event) => {
-  document.getElementById("blurMe").style.filter = "blur(6px)";
+  //document.getElementById("blurMe").style.filter = "blur(6px)";
+    document.getElementById("opacityme").style.filter = "blur(6px)";
 };
 
-window.onload = (event) => {
-  document.getElementById("opacityme").style.filter = "blur(6px)";
-};
